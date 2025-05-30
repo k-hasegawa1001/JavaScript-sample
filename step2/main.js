93a4adf26f1bfcf24f21ef820e605f57cdc33425
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-05-30 11:17:06 +0900

    (main.js) var を let/const に変更

--- step2/main.js
+++ step2/main.js
@@ -1,32 +1,32 @@
 function stopWatch(options) {
   function addMessage(message) {
-    var messageElm = document.createElement("div");
-    var now = new Date();
+    const messageElm = document.createElement("div");
+    const now = new Date();
     messageElm.innerHTML = now.getHours() + "時" + now.getMinutes() + "分" + now.getSeconds() + "秒　" + message;
     messageElm.classList = ["message"];
     logElm.appendChild(messageElm);
   }
 
   options = options || {};
-  var color = options.color || "lightblue";
-  var backgroundColor = options.backgroundColor || "black";
-  var displayElm = document.getElementsByClassName("display")[0];
+  const color = options.color || "lightblue";
+  const backgroundColor = options.backgroundColor || "black";
+  const displayElm = document.getElementsByClassName("display")[0];
   displayElm.style.color = color;
   displayElm.style.backgroundColor = backgroundColor;
 
-  var logElm = document.querySelector(".log");
-  var timer = null;
+  const logElm = document.querySelector(".log");
+  let timer = null;
 
   ///// startButton
   // startButtonというclassが付いているタグの要素のうち、
   // 最初のもの（スタートボタン）を取り出す
-  var startButton = document.getElementsByClassName("startButton")[0];
+  const startButton = document.getElementsByClassName("startButton")[0];
 
   // 取り出したstartButtonに対してクリックイベントのリスナを仕掛ける
   startButton.addEventListener("click", function () {
     // この行はクリックしたとき呼ばれる
     // console.log("start");
-    var seconds = 0;
+    let seconds = 0;
     if (timer === null) {
       timer = setInterval(function () {
         seconds++;
@@ -34,8 +34,8 @@ function stopWatch(options) {
         console.log(seconds);
       }, 1000);
 
-      // var message = "開始";
-      // var messageElm = document.createElement("div");
+      // const message = "開始";
+      // const messageElm = document.createElement("div");
       // messageElm.innerHTML = message;
       // logElm.appendChild(messageElm);
       addMessage("開始");
@@ -44,7 +44,7 @@ function stopWatch(options) {
   });
 
   ///// stopButton
-  var stopButton = document.getElementsByClassName("stopButton")[0];
+  const stopButton = document.getElementsByClassName("stopButton")[0];
   stopButton.addEventListener("click", function () {
     if (timer !== null) {
       console.log("stop:" + timer);
@@ -55,7 +55,7 @@ function stopWatch(options) {
   });
 }
 
-var options = {
+const options = {
   color: "limegreen",
   backgroundColor: "#333",
 };
