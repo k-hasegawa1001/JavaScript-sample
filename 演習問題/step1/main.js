7fff9fdd1c63ccc36766f48b47ba89651f82da58
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-06-02 00:46:59 +0900

    (練習問題2) index.html と main.js 完成

---
+++ 演習問題/step1/main.js
@@ -0,0 +1,37 @@
+class Clock {
+  init() {
+    const dateElm = document.getElementsByClassName("date")[0];
+    const timeElm = document.getElementsByClassName("time")[0];
+
+    let now = new Date();
+
+    let weekId = now.getDay();
+    let week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+    let dateMessage = `<p>${now.getFullYear()}.${now.getMonth()}.${now.getDate()} ${week[weekId]}</p>`;
+    let timeMessage = `<p>${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}</p>`;
+    dateElm.innerHTML = dateMessage;
+    timeElm.innerHTML = timeMessage;
+    setTimeout(() => {
+      this.init();
+    }, 1000);
+  }
+}
+
+// const clock = () => {
+//   const dateElm = document.getElementsByClassName("date")[0];
+//   const timeElm = document.getElementsByClassName("time")[0];
+
+//   let now = new Date();
+
+//   let weekId = now.getDay();
+//   let week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+//   let dateMessage = `<p>${now.getFullYear()}.${now.getMonth()}.${now.getDate()} ${week[weekId]}</p>`;
+
+//   dateElm.innerHTML = dateMessage;
+// };
+
+// clock();
+
+new Clock().init();
