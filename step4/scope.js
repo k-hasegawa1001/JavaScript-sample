44fe38ff2f3f17616c7f9a92371f46955d1b19f8
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-06-02 09:52:19 +0900

    4-1-01

---
+++ step4/scope.js
@@ -0,0 +1,7 @@
+if (true) {
+  const myBlockVar1 = "myBlockVar1-true"; // これがブロックスコープの変数
+  console.log(myBlockVar1);
+} else {
+  const myBlockVar1 = "myBlockVar1-false"; // これがブロックスコープの変数
+  console.log(myBlockVar1);
+}
