0f9bb58d9e6ba452d6fa3dc1e802382d0cfff01d
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-06-02 09:55:59 +0900

    4-1-02

--- step4/scope.js
+++ step4/scope.js
@@ -5,3 +5,11 @@ if (true) {
   const myBlockVar1 = "myBlockVar1-false"; // これがブロックスコープの変数
   console.log(myBlockVar1);
 }
+
+// ブロックは意図的に書くこともできる
+{
+  const myBlockVar2 = "myBlockVar2"; // これがブロックスコープの変数
+  console.log(myBlockVar2);
+}
+
+// console.log(myBlockVar2); // エラー：ブロックの外なので利用できない
