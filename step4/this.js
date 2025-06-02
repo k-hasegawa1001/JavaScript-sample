b06ccf419fb1f7748dc80192b14cd10661f214f2
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-06-02 11:27:47 +0900

    4-3-03

--- step4/this.js
+++ step4/this.js
@@ -30,3 +30,10 @@ console.log(instance1); // >= MyClass { name: "これはMyClassです" }
 instance1.test();
 // => MyClass { name: "これはMyClassです" }
 // => true
+
+const obj2 = {
+  name: "これはobj2です",
+};
+
+obj2.test = obj1.test; // obj1の関数の参照をobj2に代入
+obj2.test();
