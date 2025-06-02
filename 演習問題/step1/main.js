2655058acea8536b9bce1b0f730868526a0c8b68
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-06-02 11:51:37 +0900

    (練習問題/step1/main.js) Clockクラスの使用をclock()関数に書き換え

--- 演習問題/step1/main.js
+++ 演習問題/step1/main.js
@@ -1,21 +1,19 @@
-class Clock {
-  init() {
-    const dateElm = document.getElementsByClassName("date")[0];
-    const timeElm = document.getElementsByClassName("time")[0];
+function clock() {
+  const dateElm = document.getElementsByClassName("date")[0];
+  const timeElm = document.getElementsByClassName("time")[0];
 
-    let now = new Date();
+  let now = new Date();
 
-    let weekId = now.getDay();
-    let week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  let weekId = now.getDay();
+  let week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
-    let dateMessage = `<p>${now.getFullYear()}.${now.getMonth()}.${now.getDate()} ${week[weekId]}</p>`;
-    let timeMessage = `<p>${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}</p>`;
-    dateElm.innerHTML = dateMessage;
-    timeElm.innerHTML = timeMessage;
-    setTimeout(() => {
-      this.init();
-    }, 1000);
-  }
+  let dateMessage = `<p>${now.getFullYear()}.${now.getMonth()}.${now.getDate()} ${week[weekId]}</p>`;
+  let timeMessage = `<p>${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}</p>`;
+  dateElm.innerHTML = dateMessage;
+  timeElm.innerHTML = timeMessage;
+  setTimeout(() => {
+    clock();
+  }, 1000);
 }
 
 // const clock = () => {
@@ -34,4 +32,4 @@ class Clock {
 
 // clock();
 
-new Clock().init();
+clock();
