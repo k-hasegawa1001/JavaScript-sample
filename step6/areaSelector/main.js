986a40d75bc594a52816adabf739414ca45d2ca5
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-06-03 11:39:01 +0900

    6-4-04

--- step6/areaSelector/main.js
+++ step6/areaSelector/main.js
@@ -1,11 +1,33 @@
+const rootElm = document.getElementById("areaSelector");
+
 async function getPrefs() {
   const prefResponse = await fetch("./prefectures.json");
   return await prefResponse.json();
 }
 
-async function displayPrefs() {
-  const result = await getPrefs();
-  console.log(result);
+// async function displayPrefs() { // 削除
+//   const result = await getPrefs();
+//   console.log(result);
+// }
+
+async function updatePref() {
+  const prefs = await getPrefs();
+  createPrefOptionsHtml(prefs);
 }
 
-displayPrefs();
+function createPrefOptionsHtml(prefs) {
+  const optionStrs = [];
+  for (const pref of prefs) {
+    optionStrs.push(`
+        <option name="${pref.name}" value="${pref.code}">
+            ${pref.name}
+        </option>
+    `);
+  }
+
+  const prefSelectorElm = rootElm.querySelector(".prefectures");
+  prefSelectorElm.innerHTML = optionStrs.join("");
+}
+
+// displayPrefs(); // 削除
+updatePref();
