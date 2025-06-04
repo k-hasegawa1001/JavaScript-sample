19c45db0f7df89f644c4be262944acab04bcb588
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-06-04 10:40:52 +0900

    7-2-01

--- step7/functions.js
+++ step7/functions.js
@@ -11,3 +11,18 @@ const testFunction2 = () => {
 };
 
 testFunction2();
+
+var testValue = "test";
+(function () {
+  // この中の処理も順番に実行される
+  var testValue = "test1";
+  // ...
+})();
+console.log(testValue); // => test
+
+// これは上記の即時関数とほど同じ動作
+// letやconstはブロックスコープなのでこれだけでOK
+{
+  let testValue = "test1";
+  // ...
+}
