9f1ef355ccfffeb6cd41ca962defc7f6b23deca8
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-06-04 11:31:42 +0900

    7-4-02

--- step7/exception.js
+++ step7/exception.js
@@ -10,3 +10,37 @@ try {
 } catch (e) {
   console.error(`名前表示に失敗しました：${e.message}`);
 }
+
+class InputError extends Error {}
+
+function share(input) {
+  const value = parseInt(input);
+  if (!Number.isInteger(value)) {
+    throw new InputError("入力値が不正です");
+  }
+  if (value < 0) {
+    throw new InputError("正の整数で入力してください");
+  }
+  return divide(100, value);
+}
+
+function divide(lhv, rhv) {
+  if (rhv === 0) {
+    throw new Error("0では演算できません");
+  }
+  return lhv / rhv;
+}
+
+try {
+  const input = prompt("100円を分ける人数を入力してください", 1);
+  const result = share(input);
+  alert(`一人分は${result}円です`);
+} catch (e) {
+  if (e instanceof InputError) {
+    alert("入力値は正の整数を入れてください。リロードします");
+    location.reload();
+  } else if (e instanceof Error) {
+    console.error(e);
+    alert("予期しないエラーが発生しました。終了します");
+  }
+}
