071c60b7a503eac4efaa9c39a0b24981c45176ab
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-06-04 16:53:16 +0900

    8-3-02

--- step8/promise.js
+++ step8/promise.js
@@ -11,3 +11,15 @@ function fetchHello() {
 
   return promise.then(onFulfilled, onRejected);
 }
+
+// 実際は以下のように書くことが多い
+function fetchHello() {
+  return fetch("./hello.json").then(
+    (data) => {
+      console.log("通信成功しました");
+    },
+    (err) => {
+      console.log("通信失敗しました");
+    }
+  );
+}
