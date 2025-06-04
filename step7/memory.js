7821a9900472ebbd02575f73698195a2352c3b0f
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-06-04 16:02:03 +0900

    7-5-06

--- step7/memory.js
+++ step7/memory.js
@@ -5,3 +5,11 @@ function concatWorld(str) {
 const str1 = "Hello";
 concatWorld(str1);
 console.log(str1); // => "Hello" // 変更はされない
+
+function concatWorld(custom) {
+  custom.message = custom.message.concat("World");
+}
+
+const custom1 = { message: "Hello" };
+concatWorld(custom1);
+console.log(custom1); // => {message: "HelloWorld"}
