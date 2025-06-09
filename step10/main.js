de10be086be99cc4131ac176f29d998b4d1e06c7
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-06-09 12:15:04 +0900

    10-2-03

--- step10/main.js
+++ step10/main.js
@@ -39,24 +39,55 @@ class WordQuiz {
 
     const startBtnElm = parentElm.querySelector(".startBtn");
     startBtnElm.addEventListener("click", () => {
-      // console.log("スタートボタンがクリックされました。"); // 削除
       this.displayQuestionView();
     });
 
-    this.rootElm.appendChild(parentElm);
+    // this.rootElm.appendChild(parentElm); // 削除
+    this.replaceView(parentElm);
   }
 
   displayQuestionView() {
     const html = `
       <p>ゲームを開始しました</p>
+      <button class="retireBtn">ゲームを終了する</button>
     `;
 
     const parentElm = document.createElement("div");
     parentElm.className = "question";
     parentElm.innerHTML = html;
 
+    const retireBtnElm = parentElm.querySelector(".retireBtn");
+    retireBtnElm.addEventListener("click", () => {
+      this.displayResultView();
+    });
+
+    // this.rootElm.innerHTML = ""; // 削除
+    // this.rootElm.appendChild(parentElm); // 削除
+    this.replaceView(parentElm);
+  }
+
+  displayResultView() {
+    const html = `
+      <p>ゲーム終了</p>
+      <button class="resetBtn">開始画面に戻る</button>
+    `;
+
+    const parentElm = document.createElement("div");
+    parentElm.className = "results";
+    parentElm.innerHTML = html;
+
+    const resetBtnElm = parentElm.querySelector(".restBtn");
+    resetBtnElm.addEventListener("click", () => {
+      this.displayStartView();
+    });
+
+    this.replaceView(parentElm);
+  }
+
+  replaceView(elm) {
     this.rootElm.innerHTML = "";
-    this.rootElm.appendChild(parentElm);
+    this.rootElm.appendChild(elm);
   }
 }
+
 new WordQuiz(document.getElementById("app")).init();
