2c8fe74a9b0701d1c0adda358f2f7caf32ef5b14
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-06-09 11:14:56 +0900

    10-1-04

--- step10/main.js
+++ step10/main.js
@@ -2,5 +2,11 @@ class WordQuiz {
   constructor() {
     console.log("インスタンスが作成されたよ");
   }
+
+  async init() {
+    const response = await fetch("quiz.json");
+    this.quizData = await response.json();
+    console.log(this.quizData);
+  }
 }
-new WordQuiz();
+new WordQuiz().init();
