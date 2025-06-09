b06576463730f6161a3af9b50a9e3f9dee8d6207
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-06-09 11:29:15 +0900

    10-1-06

--- step10/main.js
+++ step10/main.js
@@ -5,14 +5,39 @@ class WordQuiz {
   }
 
   async init() {
+    await this.fetchQuizData();
+    this.displayStartView();
+  }
+
+  async fetchQuizData() {
     try {
       const response = await fetch("quiz.json");
       this.quizData = await response.json();
-      // console.log(this.quizData); // 削除
     } catch (e) {
       this.rootElm.innerText = "問題の読み込みに失敗しました";
       console.log(e);
     }
+  }
+
+  displayStartView() {
+    const levelStrs = Object.keys(this.quizData);
+    const optionStrs = [];
+    for (let i = 0; levelStrs.length > i; i++) {
+      optionStrs.push(`
+          <option value="${levelStrs[i]}" name="level">レベル${i + 1}</option>
+        `);
+    }
+
+    const html = `
+      <select class="levelSelector">
+        ${optionStrs.join("")}
+      </select>
+    `;
+
+    const parentElm = document.createElement("div");
+    parentElm.innerHTML = html;
+
+    this.rootElm.appendChild(parentElm);
   }
 }
 new WordQuiz(document.getElementById("app")).init();
