62e2be806e70185d869dac039883267cb3738348
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-06-10 13:44:47 +0900

    (効果測定/制作課題) 不要なjsファイルを削除

--- todo (効果測定)/components/src/createToDo.js
+++
@@ -1,8 +0,0 @@
-// import { ToDo } from "./classes/ToDo";
-
-// export function createToDo() {
-//   console.log("createToDo : createToDo()");
-//   let todo = new ToDo();
-
-//   const createToDoElm = todo.rootElm.querySelector(".createToDo");
-// }
